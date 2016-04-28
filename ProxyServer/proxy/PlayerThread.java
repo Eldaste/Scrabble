3af5f214fad6a002ea6ec82dd13d3438f9d32f81
keyis <selinachen16@gmail.com>
2016-04-27 21:01:06 -0500

    YOO IT WORKS

--- ProxyServer/proxy/PlayerThread.java
+++ ProxyServer/proxy/PlayerThread.java
@@ -24,6 +24,7 @@ public class PlayerThread extends Thread {
 		this.socket = proxySocket;
 	}
 
+	@SuppressWarnings("resource")
 	public void run() {
 		Log.info("Running new thread for client.");
 		DataOutputStream out = null;
@@ -32,32 +33,43 @@ public class PlayerThread extends Thread {
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			fr = new BufferedReader(new FileReader(WORDS_FILENAME));
 			out = new DataOutputStream(socket.getOutputStream());
+			PrintStream print = new PrintStream(socket.getOutputStream());
 		} catch (IOException e) {
 			Log.severe("Some issue with making IO Objects");
 			return;
 		}
+		Log.info("Successful creation of IO");
 		String line, word;
 		while (true) {
 			try {
 				line = br.readLine();
-				if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+				if ((line == null) || 
+						line.equalsIgnoreCase("SHUTDOWNSHUTDOWNSHUTDOWNSHUTDOWN")) {
 					Log.info("Closing socket and thread for client player.");
 					this.server.players.remove(Thread.currentThread());
 					socket.close();
 					return;
 				} else { // read word & check if legit
+					boolean found = false;
 					line = line.trim(); // remove white space
 					Log.info("PlayerThread, run() - reading socket: " + line);
-					while ((word = fr.readLine()) != null) {
-						word.trim();
+					while ((word = fr.readLine()) != null && !found) {
+						Log.info("Searching");
 						if (word.equals(line)) {
-							out.writeBoolean(true); // word exists
-						} else {
-							out.writeBoolean(false);
+							Log.info("Word exists!");
+							
+							found = true;
+							out.flush();
 						}
+					}
+					if (!found) {
+						out.writeBoolean(false); // word exists
+					} else {
+						out.writeBoolean(true); // word exists
 					}
 					out.flush();
 				}
+				Log.info("Out of try");
 			} catch (IOException e) {
 				e.printStackTrace();
 				return;
--- ProxyServer/test/testclient.java
+++ ProxyServer/test/testclient.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -9,31 +10,33 @@ import java.net.UnknownHostException;
 
 public class testclient {
 	static final String SERVER_ADDRESS = "localhost";
-	public static final int PORT = 2000;
+	public static final int PROXYPORT = 2000;
 
 	public static void main(String[] args) throws InterruptedException {
 		// set up socket connection to server
 		try {
-			Socket clientSocket = new Socket(SERVER_ADDRESS, PORT);
+			Socket clientSocket = new Socket(SERVER_ADDRESS, PROXYPORT);
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 			String request;
-			String response;
-			request = "zebra";
-			System.out.println("Client: " + request);
+			request = "apple";
 			out.println(request);
-			response = in.readLine(); // read the text from client
-			System.out.println("Server: " + response);
-			request = "cowboy";
 			System.out.println("Client: " + request);
+			if(in.readBoolean()) // read the text from client
+				System.out.println("Server: exists!");
+			else{
+				System.out.println("Server: NOT exists!");
+			}
+			request = "cowboyzzzz";
 			out.println(request);
-			response = in.readLine(); // read the text from client
-			System.out.println("Server: " + response);
+			System.out.println("Client: " + request);
+			if(in.read() != 0) // read the text from client
+				System.out.println("Server: exists!");
+			//System.out.println("Server: " + response);
 			request = "SHUTDOWNSHUTDOWNSHUTDOWNSHUTDOWN";
 			System.out.println("Client: " + request);
 			out.println(request);
-			response = in.readLine(); // read the text from client
-			System.out.println("Server: " + response);
+			//System.out.println("Server: " + response);
 			out.close(); // close stream
 
 		} catch (UnknownHostException e) {
