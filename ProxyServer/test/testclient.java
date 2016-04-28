f8f28fbdc4acd5fd307b44f3b45cff447351da2d
keyis <selinachen16@gmail.com>
2016-04-27 21:35:53 -0500

    now with points!

--- ProxyServer/test/testclient.java
+++ ProxyServer/test/testclient.java
@@ -18,24 +18,36 @@ public class testclient {
 			Socket clientSocket = new Socket(SERVER_ADDRESS, PROXYPORT);
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 			DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-			String request;
-			request = "apple";
-			out.println(request);
-			System.out.println("Client: " + request);
-			if(in.readBoolean()) // read the text from client
+			String word;
+			word = "apple";
+			word = word.toLowerCase().trim();
+			int wordPoints = 0;
+			out.println(word);
+			System.out.println("Client: " + word);
+			if(in.readBoolean()) {// word exists in file
 				System.out.println("Server: exists!");
-			else{
+				
+				for (int i = 0; i < word.length(); i++){
+				    char c = word.charAt(i);        
+				    wordPoints += getPoints(c);
+				}
+				System.out.println(wordPoints);
+			
+			}else{
 				System.out.println("Server: NOT exists!");
 			}
-			request = "cowboyzzzz";
-			out.println(request);
-			System.out.println("Client: " + request);
-			if(in.read() != 0) // read the text from client
+			word = "cowboyzzzz";
+			out.println(word);
+			System.out.println("Client: " + word);
+			if(in.readBoolean()) {// word exists in file
 				System.out.println("Server: exists!");
 			//System.out.println("Server: " + response);
-			request = "SHUTDOWNSHUTDOWNSHUTDOWNSHUTDOWN";
-			System.out.println("Client: " + request);
-			out.println(request);
+			}else{
+				System.out.println("Server: NOT exists!");
+			}
+			word = "SHUTDOWNSHUTDOWNSHUTDOWNSHUTDOWN";
+			System.out.println("Client: " + word);
+			out.println(word);
 			//System.out.println("Server: " + response);
 			out.close(); // close stream
 
@@ -46,4 +58,55 @@ public class testclient {
 		}
 
 	}
+	public static int getPoints(char c) {
+        int letterPoints = 0;
+
+        switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'n':
+            case 'r':
+            case 't':
+            case 'l':
+            case 'u':
+                letterPoints = 1;
+                break;
+            case 'd':
+            case 'g':
+            	letterPoints = 2;
+                break;
+            case 'b':
+            case 'c':
+            case 'm':
+            case 'p':
+            	letterPoints = 3;
+                break;
+            case 'f':
+            case 'h':
+            case 'v':
+            case 'w':
+            case 'y':
+            	letterPoints = 4;
+                break;
+            case 'k':
+            	letterPoints = 5;
+                break;
+            case 'j':
+            case 'x':
+            	letterPoints= 6;
+                break;
+            case 'q':
+            case 'z':
+            	letterPoints = 7;
+                break;
+           
+            default: 
+            	letterPoints = 0;
+                break;
+        }
+
+        return letterPoints;
+    }
 }
