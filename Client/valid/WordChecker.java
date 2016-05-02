026b167a4b793ad12d3eeb34d3bc8bb8a2b11dbc
Selina <keyis@users.noreply.github.com>
2016-05-01 20:38:26 -0500

    Update WordChecker.java

--- Client/valid/WordChecker.java
+++ Client/valid/WordChecker.java
@@ -33,6 +33,9 @@ public class WordChecker {
 		word = w.toLowerCase().trim();
 		int wordPoints = 0;
 		out.println(word);	// send word to proxy to check
+		if (word.equals("shutdownshutdownshutdownshutdown")) { // stops proxy
+			return;	
+		}
 		if(in.readBoolean()) {// proxy sends back bool
 			return true;
 		}else{
